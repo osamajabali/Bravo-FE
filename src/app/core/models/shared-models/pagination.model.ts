1d7c38bad8cc35b96c56296540486d695a7d4817
osamajabali <osamahassan22@hotmail.com>
2025-03-19 01:50:07 +0300

    .

--- src/app/core/models/shared-models/pagination.model.ts
+++ src/app/core/models/shared-models/pagination.model.ts
@@ -1,6 +1,6 @@
 export class PaginationFilter {
     pageNumber: number = 1;
-    pageSize: number = 10;
+    pageSize: number = 12;
     totalRecords: number;
     totalPages: number;
     sortColumn: string;
