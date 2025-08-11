11dd7541909e6b1527014e0206d7a67c52b1fffd
osamajabali <osamahassan22@hotmail.com>
2025-08-11 19:27:22 +0300

    envirement

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 
-const prod = "";
+const stage = "https://bravoapi.bravosoftware.com/web/";
 const dev = "http://130.61.213.6:1789/web/"
 export const environment = {
   production: false,
-  apiUrl: dev,
+  apiUrl: stage,
   debug: true,
 };
